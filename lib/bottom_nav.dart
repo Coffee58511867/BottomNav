44f85472b1aa1a9e1567b5472dc4ac92ca55806d
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-09 11:09:29 +0200

    Initial commit

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,51 @@
+import 'package:bottom_navbar/home.dart';
+import 'package:bottom_navbar/profile.dart';
+import 'package:bottom_navbar/settings.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RootPage();
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
+  List<Widget> pages = [
+    const HomePage(),
+    const SettingPage(),
+    const ProfilePage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+      ),
+      body: pages[currentPage],
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
+          NavigationDestination(icon: Icon(Icons.search), label: 'Profile')
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
