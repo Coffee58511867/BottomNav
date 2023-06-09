6032742fc98f89934b93b85a22db6008ec164fd9
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-09 11:36:25 +0200

    Initial commit

--- lib/settings.dart
+++ lib/settings.dart
@@ -9,7 +9,26 @@ class SettingPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Settings"),
       ),
-      body: const Center(child: Text("Settings Page")),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.facebook),
+            title: Text("Facebook"),
+          ),
+          ListTile(
+            leading: Icon(Icons.facebook),
+            title: Text("Facebook"),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+          ),
+          ListTile(
+            leading: Icon(Icons.call),
+            title: Text("Call"),
+          )
+        ],
+      ),
     );
   }
 }
